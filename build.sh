361835f14ba3491d2bd04354a10c77a270b16f75
David Duarte <davmduarte@ext.worten.pt>
2025-09-28 13:18:58 +0100

    In the beginning...

---
+++ build.sh
@@ -0,0 +1,26 @@
+set -e
+
+exeName="qixable"
+platform=$(uname)
+srcFiles=$(echo src/*.c)
+
+printf ".:: Building %s ::.\n\n" $exeName
+
+if [ "$platform" = "Darwin" ]; then
+    printf "\tPlatform: %s\n" "$platform"
+    printf "\tCompiling ... \n"
+    gcc -o build/$exeName $srcFiles -O2 -lraylib -std=c99 -Wall
+else
+    printf "\n\tThe %s Platform, is not supported\n" "$platform"
+    exit 1
+fi
+
+printf "\tCopying exe and assets to dist folder..\n"
+
+cp build/$exeName dist/
+cp assets/*.png dist/
+cp -R assets/categories/ dist/
+
+printf "\tDone!\n"
+
+printf "\n"
